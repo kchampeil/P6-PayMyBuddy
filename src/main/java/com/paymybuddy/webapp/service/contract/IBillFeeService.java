8bc3855af3586089f6cb521caf3874eb87152434
Karine CHAMPEIL <kchampeilpro@gmail.com>
2021-07-09 14:04:08 +0200

    ajout de l'interface IBillFeeService qui permettra la mise en place du service de facturation en V2

---
+++ src/main/java/com/paymybuddy/webapp/service/contract/IBillFeeService.java
@@ -0,0 +1,8 @@
+package com.paymybuddy.webapp.service.contract;
+
+//TODO V2 implémenter l'interface lors de la mise en place du système de facturation
+public interface IBillFeeService {
+
+    void billFeesToUser(Long userId);
+
+}
