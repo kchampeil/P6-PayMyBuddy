6291a8eb13822bd5454bbb711814a240d98e581e
Karine CHAMPEIL <kchampeilpro@gmail.com>
2021-07-09 14:58:00 +0200

    retrait d'imports inutiles

--- src/main/java/com/paymybuddy/webapp/service/PMBUserDetailsService.java
+++ src/main/java/com/paymybuddy/webapp/service/PMBUserDetailsService.java
@@ -6,14 +6,11 @@ import com.paymybuddy.webapp.model.PMBUserDetails;
 import com.paymybuddy.webapp.service.contract.IUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.HashSet;
 
 @Slf4j
--- src/test/java/com/paymybuddy/webapp/testconstants/RelationshipTestConstants.java
+++ src/test/java/com/paymybuddy/webapp/testconstants/RelationshipTestConstants.java
@@ -1,7 +1,5 @@
 package com.paymybuddy.webapp.testconstants;
 
-import java.math.BigDecimal;
-
 public class RelationshipTestConstants {
 
     public static final Long EXISTING_RELATIONSHIP_ID = 1L;
